fccbc052c018320e6ca04db8f8f2bc0272fece01
Nicole Sanders <nsanders9022@gmail.com>
2017-04-07 09:24:21 -0700

    show the 6 hard coded players on the homepage with their name, position, and image

---
+++ src/app/players.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Players } from './players.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+
+@Injectable()
+export class PlayersService {
+  players: FirebaseListObservable<any[]>;
+
+  constructor(private angularFire: AngularFire) {
+    this.players = angularFire.database.list('players');
+  }
+
+  getPlayers() {
+    return this.players;
+  }
+
+}
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Players } from '../players.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { PlayersService } from '../players.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
-  styleUrls: ['./players.component.css']
+  styleUrls: ['./players.component.css'],
+  providers: [PlayersService]
 })
 export class PlayersComponent implements OnInit {
+  players: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private playersService: PlayersService) { }
 
   ngOnInit() {
+    this.players = this.playersService.getPlayers();
   }
 
 }
