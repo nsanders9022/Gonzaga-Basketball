7a29c3be04b73bae74de90d589e163d9e9966994
Nicole Sanders <nsanders9022@gmail.com>
2017-04-07 08:26:02 -0700

    add a team component and a route. Update app.module and create app.routing.ts file with the route for the team component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
